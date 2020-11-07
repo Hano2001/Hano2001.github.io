967fe9cc3066eaf2cdfb67fb0dddec2a9a143697
Harald <Harald.Norlin@yh.nackademin.se>
2020-11-07 13:43:19 +0100

    renamed variables in js and removed unnecessary code

--- app/script.js
+++ app/script.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let nackaLink = document.getElementById("navNACKA");
   let someLink = document.getElementById("navSOME");
   let navDivs = document.getElementsByClassName("navDiv");
-  console.log(typeof navDivs);
+
   hideAllDivs(navDivs[0]);
 
-  function hideAllDivs(x) {
+  function hideAllDivs(activeDiv) {
     for (let i = 0; i < navDivs.length; i++) {
       navDivs[i].classList.add("hide");
-      x.classList.remove("hide");
+      activeDiv.classList.remove("hide");
     }
   }
 
